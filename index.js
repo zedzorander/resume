da2ab75dd92de98ba9818d28ac973074edd0947c
zedzorander <colephares@gmail.com>
2021-07-02 20:43:44 -0700

    First commit

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
 $(document).ready(function() {
-    $("#hamburger").on("click", function() {
-        $("i").toggle(function() {
-            var menuWidth = $("#menu").width() == 500 ? "100px" : "500px";
-            $("#menu").animate({ width: menuWidth });
-            
-        })
+    var menuTitle = $("#menuTitle")
+    var links = $("#links")
+    links.hide()
+    var bars = $("#bars-lg")
+    var times = $("#times-lg")
+    times.hide()
+    var barsSm = $("#bars-sm")
+    var timesSm = $("#times-sm")
+    timesSm.hide()
+    var toggle = false
+    var toggleSm = false
+
+    $("#hamburger-sm").on("click", function() {
+        toggleSm = !toggleSm
+        if(toggleSm) {
+            barsSm.hide(600)
+            timesSm.show(600)
+        } else {
+            timesSm.hide(600)
+            barsSm.show(600)
+        }
     })
+
+    $("#hamburger-lg").on("click", function() {
+        // Changes width of menu bar
+        var menuWidth = $("#menu").width() == 700 ? "100px" : "700px";
+        $("#menu").animate({ width: menuWidth });
+
+        // TOGGLES HAMBURGER ICONS AND MENU ELEMENTS
+        toggle = !toggle
+        if(toggle) {
+            bars.hide(600)
+            times.show(600)
+            menuTitle.hide(600)
+            links.show(600)
+        } else {
+            times.hide(600)
+            bars.show(600)
+            links.hide(600)
+            menuTitle.show(600)
+        }
+    })
+
+
 })
 
 
